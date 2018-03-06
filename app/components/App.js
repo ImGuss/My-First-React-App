db7e09ecb05b389e2d0596a5d879a509f46e8d8e
Gustavo Gutierrez <imguss@gmail.com>
2018-03-05 23:25:04 -0800

    initial setup for first react app

---
+++ app/components/App.js
@@ -0,0 +1,16 @@
+const React = require('react');
+const ReactDOM = require('react-dom');
+
+
+class App extends React.Component {
+  render() {
+    return (
+      <p>
+        This is a paragraph!!!!
+      </p>
+    );
+  }
+}
+
+
+module.exports = App;
